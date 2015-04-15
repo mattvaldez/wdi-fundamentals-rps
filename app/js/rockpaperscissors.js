6e476152fc98d2ab92c6439204e9d595ad69f99f
Matthew Valdez <matvaldez@gmail.com>
2015-04-14 22:52:38 -0400

    javaScript code to determine winner as per Unit 4 HW

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -22,25 +22,49 @@ function randomPlay() {
 ////////////////////////////////////////////////
 
 function getPlayerMove(move) {
-    return move || getInput()
+    
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
-    return || randomPlay()
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
-}
+    return move || randomPlay();
 
 function getWinner(playerMove,computerMove) {
     var winner;
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if((playerMove = 'rock') && (computerMove = 'scissors')) 
+        winner = 'player';
+    
+    else if((playerMove = 'rock') && (computerMove = 'paper'))
+        winner = 'computer';
+    
+    else if((playerMove = 'rock') && (computerMove = 'rock'))          
+        winner = 'tie';
+    
+    else if ((playerMove = 'scissors') && (computerMove = 'rock'))
+        winner = 'computer';
+    
+    else if((playerMove = 'scissors') && (computerMove = 'paper'))
+        winner= 'player';
+    
+    else if((playerMove = 'scissors') && (computerMove = 'scissors'))    
+        winner = 'tie';
+    
+    else if((playerMove = 'paper') && (computerMove = 'scissors'))  
+        winner = 'computer';
+    
+    else if((playerMove = 'paper') && (computerMove = 'rock'))         
+        winner = 'player';
+    
+    else if((playerMove = 'paper') && (computerMove = 'paper'))
+        winner = 'tie';
+    
     return winner;
 }
 
